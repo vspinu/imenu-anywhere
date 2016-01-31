573b8fcb6dd6f02233e7abec903a350e443020c1
Bozhidar Batsov <bozhidar@batsov.com>
2016-01-31 10:27:36 +0200

    Fix a couple of checkdoc issues

--- imenu-anywhere.el
+++ imenu-anywhere.el
@@ -25,8 +25,8 @@
 ;;; Commentary:
 ;;
 ;; `imenu-anywhere` command pops an IDO interface with all the imenu tags across
-;; all buffers with the same mode as the current one. In a sense it is similar
-;; to etag selection, but works only for the open buffers. This is often more
+;; all buffers with the same mode as the current one.  In a sense it is similar
+;; to etag selection, but works only for the open buffers.  This is often more
 ;; convenient as you don't have to explicitly build the etags table.
 ;;
 ;; To activate, just bind imenu-anywhere to a convenient key:
@@ -34,7 +34,7 @@
 ;;    (global-set-key (kbd "C-.") 'imenu-anywhere)
 ;;
 ;; There is also `helm-imenu-anywhere` which is like imenu-anywhere but uses
-;; helm (https://github.com/emacs-helm) interface instead of IDO. Helm library
+;; helm (https://github.com/emacs-helm) interface instead of IDO.  Helm library
 ;; is not loaded by imenu-anywhere.el and you have to install helm separately.
 
 ;;; Code:
